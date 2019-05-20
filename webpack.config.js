f46467af36eea90ee6b09966fbd5cf30a4d2163f
NetoMarcal <netodevce@gmail.com>
2019-05-20 20:57:40 -0300

    Instalado e configurado html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/app.js',
     output: {
         path: __dirname + '/build',
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+           template: './src/index.html',
+        })
+    ]
 }
